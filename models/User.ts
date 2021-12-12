7fa35f9c7064ddcbf67b88d2bfab9279fb919a5d
ChaFFee2142 <spboiko@gmail.com>
2021-12-12 14:59:37 +0100

    User model js to ts refactor

--- models/User.ts
+++ models/User.ts
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose')
 
+enum Gender {
+    male = 'male',
+    female = 'female',
+    other = 'other'
+}
+
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        min: 3,
-        max: 20,
-        unique: true
+        min: 2,
+        max: 20
+    },
+    userSurname: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 50
     },
     email: {
         type: String,
@@ -30,6 +41,12 @@ const UserSchema = new mongoose.Schema({
     following: {
         type: Array,
         default: []
+    },
+    gender: {
+        type: String,
+        enum: Gender,
+        default: Gender.other
+
     },
     isAdmin: {
         type: Boolean,
